ac6cfd91759794b17f4d67cbbf9409e3f520d2aa
MathijsVeldmeijer <144016735+MathijsVeldmeijer@users.noreply.github.com>
2024-06-05 16:03:30 +0200

    werk alsjebliegf

---
+++ untitled/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+class Product
+{
+
+}
+
+
+$chair1 = new Product();
+$chair2 = new Product();
+$table1 = new Product();
+$lamp1 = new Product();
+
+var_dump($chair1);
+var_dump($chair2);
+var_dump($table1);
+var_dump($lamp1);
