e459e118bab25c686d6d24f2254409f39840c75e
MathijsVeldmeijer <144016735+MathijsVeldmeijer@users.noreply.github.com>
2024-06-05 16:13:26 +0200

    week2 bezig

--- untitled/Product.php
+++ untitled/Product.php
@@ -2,7 +2,7 @@
 
 class Product
 {
-
+    public $name;
 }
 
 
@@ -15,3 +15,16 @@ var_dump($chair1);
 var_dump($chair2);
 var_dump($table1);
 var_dump($lamp1);
+
+
+
+$game1 = new Product();
+$game1->name = "Team Fortress 2";
+$game2 = new Product();
+$game2->name = "Rainbow Six Siege";
+
+
+var_dump($game1);
+var_dump($game2);
+
+echo $game1->name;
